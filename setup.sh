758ea94e8c0bc9742b42a7fc9491d5139b540ff9
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-01 17:08:15 +0100

    set up file populated

--- setup.sh
+++ setup.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 
+clear
+echo "============ Setting up Your Shellmon platform ================="
+sleep 2
+apt update && apt upgrade -y
+apt install mosquitto mosquitto-clients -y
