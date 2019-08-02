ef1d174e3f50713c4b980216fb4dfa825b814b45
Emeka Ugwuanyi <emylincon@gmail.com>
2019-08-02 16:31:26 +0100

    set up improved

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ echo "============ Setting up Your Shellmon platform ================="
 sleep 2
 apt update && apt upgrade -y
 apt install mosquitto mosquitto-clients -y
+touch /etc/mosquitto/passwd ; mosquitto_passwd -b /etc/mosquitto/passwd shellmon shellmon
+echo -e 'allow_anonymous false \npassword_file /etc/mosquitto/passwd' > /etc/mosquitto/conf.d/default.conf
 /etc/init.d/mosquitto start
 apt install python3 -y
 apt install python3-pip -y
